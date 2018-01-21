ed55f893ecfa56c1b5f7c78be7ce7e9ec54a60fa
Arianit Hetemi <nitihetemi@gmail.com>
2018-01-21 15:36:04 +0100

    Base Flask testing and testing endpoints added

--- test.py
+++ test.py
@@ -1,8 +1,39 @@
-import unittest
+import unittest, json
+from flask_testing import TestCase
+from app import create_app, db
 
-def fun(x):
-    return x + 1
+class BaseTestCase(TestCase):
 
-class MyTest(unittest.TestCase):
-    def test(self):
-        self.assertEqual(fun(3), 4)
+    ############################
+    #### setup flask app and teardown ####
+    ############################
+
+    def create_app(self):
+        return create_app()
+
+    # executed prior to each test
+    def setUp(self):
+        db.create_all()
+
+    # executed after each test
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+
+###############
+#### tests ####
+###############
+
+class FlaskTestCase(BaseTestCase):
+
+    def test_index(self):
+        response = self.client.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_main(self):
+        response = self.client.get('/', follow_redirects=True)
+        self.assertIn(b'Welcome to the Flask App.', response.data)
+
+if __name__ == "__main__":
+    unittest.main()
